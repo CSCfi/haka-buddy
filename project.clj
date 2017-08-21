b41aa55c508f0b85cea588682f9301e165b58d11
tjalkane <tjalkane@users.noreply.github.com>
2017-08-21 13:22:16 +0300

    bump up buddy-auth dependency

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/CSCfi/haka-buddy"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[buddy/buddy-auth "1.4.1"]
+  :dependencies [[buddy/buddy-auth "2.0.0"]
                  [org.clojure/clojure "1.8.0"]])
