5621bcc2fc2d6e3c484032441fc5407070fcc028
tjalkane <tjalkane@users.noreply.github.com>
2018-03-02 15:56:32 +0200

    update clojure version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject haka-buddy "0.2.2"
+(defproject haka-buddy "0.2.3"
   :description "Haka SP based authentication and authorization backend for Buddy-Auth library"
   :url "https://github.com/CSCfi/haka-buddy"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[buddy/buddy-auth "2.1.0"]
-                 [org.clojure/clojure "1.8.0"]])
+                 [org.clojure/clojure "1.9.0"]])
